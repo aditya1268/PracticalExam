0c85c7a49b696ac06af5963822f34931cc487973
aditya1268 <152869420+aditya1268@users.noreply.github.com>
2024-05-06 10:19:56 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("HELLO WORLD")
+def linearSearch(key):
+  a=[]
+  a.append(2);
+  a.append(3);
+  a.append(4);
+  a.append(5);
+  a.append(6);
+  a.append(7);
+  for i in range(len(a)):
+    if(a[i]==key):
+      s=1
+      break
+  if(s==1):
+    print("Key Found")
+  else:
+    print("Key Not Found")
+linearSearch(1)
